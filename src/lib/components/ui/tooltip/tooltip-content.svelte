9e68022066eab0e0006b80699e8b505bbe5fec3d
Hunter Johnston <johnstonhuntera@gmail.com>
2024-05-29 20:07:24 -0400

    init

---
+++ src/lib/components/ui/tooltip/tooltip-content.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Tooltip as TooltipPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils.js';
+
+	type $$Props = TooltipPrimitive.ContentProps;
+
+	let className: $$Props['class'] = undefined;
+	export let sideOffset: $$Props['sideOffset'] = 4;
+	export { className as class };
+</script>
+
+<TooltipPrimitive.Content
+	{sideOffset}
+	class={cn(
+		'z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md',
+		className
+	)}
+	{...$$restProps}
+>
+	<slot />
+</TooltipPrimitive.Content>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import pkgJson from '../../package.json';
+</script>
+
+<div class="flex flex-col items-start gap-8 p-40">
+	<h1 class="text-lg font-semibold">Svelte Version: {pkgJson.devDependencies.svelte}</h1>
+	<Tooltip.Root>
+		<Tooltip.Trigger>Hover me a few times to reproduce the issue</Tooltip.Trigger>
+		<Tooltip.Content>Hello world</Tooltip.Content>
+	</Tooltip.Root>
+
+	<p class="text-xs">Change Svelte to: &gtnext-140 to see the issue</p>
+</div>
